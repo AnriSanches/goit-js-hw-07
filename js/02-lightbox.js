a2953ea9915a86532a1692b98d2a804718cd44ea
AnriSanches <89216270+AnriSanches@users.noreply.github.com>
2022-02-14 23:50:52 +0200

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+
+function createGallery () {
+    const addMarkup = galleryItems.map(({ preview, original, description }) => `
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`).join('');
+    
+    galleryContainer.insertAdjacentHTML('beforeend', addMarkup);
+};
+createGallery();
+
+let lightbox = new SimpleLightbox('a', {
+    captions: true,
+    // captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    loop: true,
+    scrollZoom: true,
+    scrollZoomFactor: 0.001,
+});
+// var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
